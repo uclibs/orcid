f123842e8bf64e010383fd30688399d0a5c91735
Lukas Miciunas <miciunla@mail.uc.edu>
2016-12-12 14:18:37 -0500

    Adds a disconnect from orcid feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Orcid::Engine.routes.draw do
   scope module: 'orcid' do
     resource :profile_request, only: [:show, :new, :create, :destroy]
     resources :profile_connections, only: [:new, :create, :index]
+    get "disconnect", to: "profile_connections#destroy"
   end
 end
